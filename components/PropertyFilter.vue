0e134f889d9eea50c5e92b055bcd66608fb7611b
Tarcio Saraiva <tarcio.saraiva@afterpaytouch.com>
2021-11-09 11:46:20 +1100

    add more price breakdowns

--- components/PropertyFilter.vue
+++ components/PropertyFilter.vue
@@ -73,16 +73,37 @@
             <label for="p_max_price">Max price</label>
             <select id="p_max_price" v-model="pMaxPrice">
               <option value="500000">500 K</option>
+              <option value="550000">550 K</option>
               <option value="600000">600 K</option>
+              <option value="650000">650 K</option>
               <option value="700000">700 K</option>
+              <option value="750000">750 K</option>
               <option value="800000">800 K</option>
+              <option value="850000">850 K</option>
               <option value="900000">900 K</option>
+              <option value="950000">950 K</option>
               <option value="1000000">1 M</option>
               <option value="1100000">1.1 M</option>
               <option value="1200000">1.2 M</option>
               <option value="1300000">1.3 M</option>
               <option value="1400000">1.4 M</option>
               <option value="1500000">1.5 M</option>
+              <option value="1600000">1.6 M</option>
+              <option value="1700000">1.7 M</option>
+              <option value="1800000">1.8 M</option>
+              <option value="1900000">1.9 M</option>
+              <option value="2000000">2 M</option>
+              <option value="2250000">2.25 M</option>
+              <option value="2500000">2.5 M</option>
+              <option value="2750000">2.75 M</option>
+              <option value="3000000">3 M</option>
+              <option value="4000000">4 M</option>
+              <option value="5000000">5 M</option>
+              <option value="6000000">6 M</option>
+              <option value="7000000">7 M</option>
+              <option value="8000000">8 M</option>
+              <option value="9000000">9 M</option>
+              <option value="10000000">10 M</option>
             </select>
           </div>
         </div>
