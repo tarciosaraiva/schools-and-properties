3592aed14c0feb0fba9ed8a9e6611e8ad8a44d35
Tarcio Saraiva <tarcio.saraiva@afterpaytouch.com>
2021-11-28 14:56:03 +1100

    add link to 'Better Education' from schools without rating

--- components/school/SchoolPopupContent.vue
+++ components/school/SchoolPopupContent.vue
@@ -5,8 +5,8 @@
       <br>
       <small>{{ school.educationSector }}, {{ school.schoolType }} - {{ school.phoneNumber }}</small>
     </p>
-    <p v-if="hasPrimaryRating">Primary ratings</p>
-    <dl v-if="hasPrimaryRating" class="school-stats">
+    <p v-if="isPrimary && hasPrimaryRating">Primary ratings</p>
+    <dl v-if="isPrimary && hasPrimaryRating" class="school-stats">
       <dt class="label">Overall rating</dt>
       <dd class="value"><em>{{ school.primaryOverallScore }}</em></dd>
       <dt class="label">English rating</dt>
@@ -16,8 +16,12 @@
       <dt class="label">Enrolments</dt>
       <dd class="value"><em>{{ school.primaryEnrolments }}</em></dd>
     </dl>
-    <p v-if="hasSecondaryRating">Secondary ratings</p>
-    <dl v-if="hasSecondaryRating" class="school-stats">
+    <br v-if="isPrimary && !hasPrimaryRating" />
+    <p v-if="isPrimary && !hasPrimaryRating">Check school rating at
+      <a href="https://www.bettereducation.com.au/school/Primary/vic/vic_primary_school_rating.aspx" target="_blank">Better Education</a>.
+    </p>
+    <p v-if="isSecondary && hasSecondaryRating">Secondary ratings</p>
+    <dl v-if="isSecondary && hasSecondaryRating" class="school-stats">
       <dt class="label">Overall rating</dt>
       <dd class="value"><em>{{ school.secondaryOverallScore }}</em></dd>
       <dt class="label">English rating</dt>
@@ -27,13 +31,15 @@
       <dt class="label">Enrolments</dt>
       <dd class="value"><em>{{ school.secondaryEnrolments }}</em></dd>
     </dl>
+    <br v-if="isSecondary && !hasSecondaryRating" />
+    <p v-if="isSecondary && !hasSecondaryRating">Check school rating at
+      <a href="https://www.bettereducation.com.au/school/secondary/vic/vic_secondary_school_rating.aspx" target="_blank">Better Education</a>.
+    </p>
   </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
-
-import { School } from '~/store'
 
 export default Vue.extend({
   props: {
@@ -43,11 +49,21 @@ export default Vue.extend({
     }
   },
   computed: {
+    isPrimary () {
+      const primSchool = this.school
+      return primSchool.schoolType === 'Primary' || primSchool.schoolType === 'Pri/Sec'
+    },
+    isSecondary () {
+      const secSchool = this.school
+      return secSchool.schoolType === 'Secondary' || secSchool.schoolType === 'Pri/Sec'
+    },
     hasPrimaryRating () {
-      return (this.school as School).primaryOverallScore !== '#N/A'
+      const primSchool = this.school
+      return primSchool.primaryOverallScore !== '#N/A'
     },
     hasSecondaryRating () {
-      return (this.school as School).secondaryOverallScore !== '#N/A'
+      const secSchool = this.school
+      return secSchool.secondaryOverallScore !== '#N/A'
     }
   }
 })
